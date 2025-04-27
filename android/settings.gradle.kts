8bf3fa5d312830aa978bd5e00d74879556135040
Ashish Khandelwal <ashishkhandelwal01@hotmail.com>
2025-04-27 11:30:06 -0400

    Revert "github action try"
    This reverts commit 0cdbd3d2a7eaa5b3b4169a0cf05164d3cf59b44b.

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -10,15 +10,14 @@ pluginManagement {
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
     repositories {
-        google()  // Ensure Google's Maven repository is included
-        mavenCentral()  // Ensure Maven Central is included
-        gradlePluginPortal()  // Ensure Gradle Plugin Portal is included
-        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }  // Adding Flutter-specific Maven repository
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0" // Ensure correct version
+    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false
 }
