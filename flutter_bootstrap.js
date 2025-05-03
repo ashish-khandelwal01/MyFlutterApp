ea4f5628446e5ed7978df2a716cc6dc7ce884eb0
ashish-khandelwal01 <ashish-khandelwal01@users.noreply.github.com>
2025-05-03 21:03:00 +0000

    deploy: bca1e748970dec5691e8520517670d00a970e77a

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "5604940"
+    serviceWorkerVersion: "2043730019"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"assets/AssetManifest.json": "fe4a9246a22b1d233333bd9314387b9
 "canvaskit/skwasm_st.js.symbols": "c7e7aac7cd8b612defd62b43e3050bdd",
 "index.html": "42688438ff69e7ba74db7268eaed4b9e",
 "/": "42688438ff69e7ba74db7268eaed4b9e",
-"flutter_bootstrap.js": "b67f82b7379deffc86459b306e4993f6",
+"flutter_bootstrap.js": "5e1136ba03ed9fd321dbba5ef442aa69",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
